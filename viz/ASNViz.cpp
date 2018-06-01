b23bf03f46eadd0c6472d4e0544e79f529ea0f86
Steffen Planthaber <Steffen.Planthaber@dfki.de>
2018-06-01 14:46:40 +0200

    compiling viz

--- viz/ASNViz.cpp
+++ viz/ASNViz.cpp
@@ -1,45 +1,41 @@
 #include <iostream>
 #include "ASNViz.hpp"
 
+#include <vizkit3d/Vizkit3DWidget.hpp>
+
 using namespace vizkit3d;
 
-struct ASNViz::Data {
-    // Copy of the value given to updateDataIntern.
-    //
-    // Making a copy is required because of how OSG works
-    ASNTYPE data;
+Asn1SccRigidBodyStateViz::Asn1SccRigidBodyStateViz(){
+
+    // Create and add plugin
+    plugin = dynamic_cast<VizPluginBase*>(getWidget()->loadPlugin("base", "RigidBodyStateVisualization"));
+    if (NULL == plugin)
+    {
+        throw std::runtime_error("loading plugin BodyStateVisualization from base failed");
+        }
+
 };
-
-
-template <ASNTYPE,ROCKTYPE> ASNViz::ASNViz()
-    : p(new Data)
-{
-}
-
-ASNViz::~ASNViz()
-{
-    delete p;
-}
-
-osg::ref_ptr<osg::Node> ASNViz::createMainNode()
-{
-    // Geode is a common node used for vizkit3d plugins. It allows to display
-    // "arbitrary" geometries
-    return new osg::Geode();
-}
-
-void ASNViz::updateMainNode ( osg::Node* node )
-{
-    osg::Geode* geode = static_cast<osg::Geode*>(node);
-    // Update the main node using the data in p->data
-}
-
-void ASNViz::updateDataIntern( const PoseWrapper::Pose3D &value)
-{
-    p->data = value;
-    std::cout << "got new sample data" << std::endl;
-}
-
 //Macro that makes this plugin loadable in ruby, this is optional.
-VizkitQtPlugin(ASNViz)
+VizkitQtPlugin(Asn1SccRigidBodyStateViz)
+
+// osg::ref_ptr<osg::Node> ASNViz::createMainNode()
+// {
+//     // Geode is a common node used for vizkit3d plugins. It allows to display
+//     // "arbitrary" geometries
+//     return new osg::Geode();
+// }
+
+// void ASNViz::updateMainNode ( osg::Node* node )
+// {
+//     osg::Geode* geode = static_cast<osg::Geode*>(node);
+//     // Update the main node using the data in p->data
+// }
+
+// void ASNViz::updateDataIntern( const PoseWrapper::Pose3D &value)
+// {
+//     p->data = value;
+//     std::cout << "got new sample data" << std::endl;
+// }
+
+
 
--- viz/ASNViz.hpp
+++ viz/ASNViz.hpp
@@ -2,33 +2,61 @@
 #define ASN_Viz_ASNViz_H
 
 #include <boost/noncopyable.hpp>
-#include <vizkit3d/Vizkit3DPlugin.hpp>
+#include <vizkit3d/RigidBodyStateVisualization.hpp>
 #include <osg/Geode>
-#include <ASN.1/BaseTypes.hpp>
-#include <ASN.1/Pose.hpp>
+//#include <ASN.1/BaseTypes.hpp>
+//#include <ASN.1/Pose.hpp>
+
+#include "asn1/RigidBodyState.h"
+#include <base_support/asn1RigidBodyStateConvert.hpp>
+
+#include "PluginNames.hpp"
+
+//#define ADD_TYPES()
 
 namespace vizkit3d
 {
-    template <ASNTYPE,ROCKTYPE> class ASNViz
-        : public vizkit3d::Vizkit3DPlugin< ASNTYPE >
-        , boost::noncopyable
+    class Asn1SccRigidBodyStateViz: public vizkit3d::Vizkit3DPlugin< asn1SccRigidBodyState >, boost::noncopyable
     {
-    Q_OBJECT
-    public:
-        ASNViz();
-        ~ASNViz();
+        Q_OBJECT
+        public:
+            Asn1SccRigidBodyStateViz();
+            virtual ~Asn1SccRigidBodyStateViz(){};
 
-    Q_INVOKABLE void updateData( const ASNTYPE &sample)
-    {vizkit3d::Vizkit3DPlugin< ASNTYPE >::updateData(sample);}
+        Q_INVOKABLE void updateData( const asn1SccRigidBodyState &sample){
+            //convert
+            base::samples::RigidBodyState rbs;
+            RigidBodyState_fromAsn1(rbs,sample);
+            dynamic_cast<vizkit3d::RigidBodyStateVisualization*>(plugin)->updateData(rbs);
+        }
 
-    protected:
-        virtual osg::ref_ptr<osg::Node> createMainNode();
-        virtual void updateMainNode(osg::Node* node);
-        virtual void updateDataIntern( const& plan);
-        
-    private:
-        struct Data;
-        Data* p;
+        virtual void updateDataIntern(const asn1SccRigidBodyState &data){
+            //not used or convert??
+            //base::samples::RigidBodyState rbs;
+            //dynamic_cast<vizkit3d::RigidBodyStateVisualization*>(plugin)->updateDataIntern(rbs);
+
+        }
+
+        virtual void updateMainNode(osg::Node* node){
+            //vizkit3d::RigidBodyStateVisualization::updateMainNode(node);
+        }
+
+        // virtual osg::ref_ptr<osg::Node> createMainNode()
+        // {
+        //     // Geode is a common node used for vizkit3d plugins. It allows to display
+        //     // "arbitrary" geometries
+        //     return plugin->createMainNode();
+        // }
+
+        // virtual void updateMainNode ( osg::Node* node )
+        // {
+        //     return plugin->updateMainNode(node);
+        // }
+
+        private:
+
+            VizPluginBase* plugin;
+
     };
 }
 #endif
