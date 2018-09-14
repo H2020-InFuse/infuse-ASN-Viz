4672e58450cb8c3a7757c25f599bde62af8f89fa
Bilal Wehbe <bilal.wehbe@dfki.de>
2018-09-14 15:17:32 +0200

    updated maximum number of points in trajectory and line width

--- viz/TrajectoryViz.hpp
+++ viz/TrajectoryViz.hpp
@@ -11,12 +11,14 @@ namespace vizkit3d
     {
         Q_OBJECT
         public:
-            TrajectoryViz(){qRegisterMetaType<asn1SccVector3d>();};
+            TrajectoryViz(){qRegisterMetaType<asn1SccVector3d>();
+                            setMaxNumberOfPoints(100000);
+                            setLineWidth(4.0);};
             virtual ~TrajectoryViz(){};
         Q_INVOKABLE void updateData( const asn1SccVector3d &sample){
             base::Vector3d rocktype;
             Vector3d_fromAsn1(rocktype,sample);
-            vizkit3d::TrajectoryVisualization::updateData(rocktype);
+            vizkit3d::TrajectoryVisualization::updateTrajectory(rocktype);
         }
         virtual void updateDataIntern(const asn1SccVector3d &data){}
     };
