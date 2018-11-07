79eb325972ccd9504070dd1e203ac903d272cc14
Steffen Planthaber <Steffen.Planthaber@dfki.de>
2018-11-07 18:21:25 +0100

    use rigidBodystate viz for asn1SccTransformWithCovariance

--- viz/RigidBodyStateViz.hpp
+++ viz/RigidBodyStateViz.hpp
@@ -2,6 +2,8 @@
 
 #include <vizkit3d/RigidBodyStateVisualization.hpp>
 #include <base_support/asn1RigidBodyStateConvert.hpp>
+#include <base_support/asn1TransformWithCovarianceConvert.hpp>
+
 
 Q_DECLARE_METATYPE(asn1SccRigidBodyState);
 
@@ -16,6 +18,11 @@ namespace vizkit3d
         Q_INVOKABLE void updateData( const asn1SccRigidBodyState &sample){
             base::samples::RigidBodyState rocktype;
             RigidBodyState_fromAsn1(rocktype,sample);
+            vizkit3d::RigidBodyStateVisualization::updateData(rocktype);
+        }
+        Q_INVOKABLE void updateData( const asn1SccTransformWithCovariance &sample){
+            base::TransformWithCovariance rocktype;
+            TransformWithCovariance_fromAsn1(rocktype,sample);
             vizkit3d::RigidBodyStateVisualization::updateData(rocktype);
         }
         virtual void updateDataIntern(const asn1SccRigidBodyState &data){}
